8e6c87f989d530e1b6c7c2617d5f770bfec90029
ProSolver-org <poralekha@prosolver.org>
2023-04-03 08:05:09 +0600

    0=>Fix cors error

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -3,14 +3,15 @@ export default function Posts() {
   const [post, getPost] = useState([])
   //const API = 'https://jsonplaceholder.typicode.com/posts';
   //const API = 'http://127.0.0.1:8000/api/questions';
-//const API = 'http://127.0.0.1:8000/api/questions';
+/*const API = 'http://127.0.0.1:8000/api/questions';*/
+const API = 'http://localhost:8000/api/questions';
 //const API = 'https://poralekh.prosolver.org/poralekha/api/questions';
-const API ='https://poralekha.prosolver.org/poralekha/api/students';
+//const API ='https://poralekha.prosolver.org/poralekha/api/students';
   const fetchPost = () => {
     fetch(API)
-      .then((res) => res.json())
-      .then((res) => {
-        //console.log(res)
+    .then((res) => res.json())
+    .then((res) => {
+        console.log(res)
         getPost(res)
       })
   }
@@ -24,7 +25,8 @@ const API ='https://poralekha.prosolver.org/poralekha/api/students';
         {post.map((item, i) => {
           //return <li key={i}>{item.title}</li>
           //return <li key={i}>{item.question}</li>
-          return<li key={i}>{item.studentname}</li>
+          //return<li key={i}>{item.studentname}</li>
+          return <li key={i}>{item.question}</li>
         })}
       </ul>
     </>
